eb4d2a492eaeda781736b183ec1a9d50da53af22
afrofarms144-stack <afrofarms144@gmail.com>
2025-09-18 02:36:10 +0300

    adding cors

--- build/main.js
+++ build/main.js
@@ -7599,11 +7599,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  return next();
-});
 app.use(cors());
 app.set("view engine", "ejs"); //admin routes
 
--- server.js
+++ server.js
@@ -33,15 +33,6 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  return next();
-});
-
 app.use(cors());
 
 app.set("view engine", "ejs");
